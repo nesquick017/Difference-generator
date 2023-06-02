297fd150ee32c48271b99b7c9f31c22eb07f13ab
Madorskii <madorskiy.017@yandex.ru>
2023-06-02 15:22:17 -0300

    tests updated

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,10 +1,29 @@
 /* eslint-disable no-undef */
 import { flat } from '../__fixtures__/result.js';
 import makeCompare from '../src/idex.js';
+import { getFile, getType } from '../src/idex.js';
+import fs from 'fs';
 
 const fileName1 = 'file1.json';
 const fileName2 = 'file2.json';
-const result = flat;
+const resultMakeCompare = flat;
+const resultGetFile = fs.readFileSync(
+    '/Users/nikita_mac/Documents/frontend-project-46/__fixtures__/file1.json',
+    'utf-8'
+);
+
 test('makeCompare should return the difference', () => {
-    expect(makeCompare(fileName1, fileName2)).toEqual(result);
+    expect(makeCompare(fileName1, fileName2)).toEqual(resultMakeCompare);
+});
+
+test('getType should give us a type of file', () => {
+    expect(getType('file1.json')).toEqual('json');
+});
+
+test('readFile should give us a proper file', () => {
+    expect(
+        getFile(
+            '/Users/nikita_mac/Documents/frontend-project-46/__fixtures__/file1.json'
+        )
+    ).toEqual(resultGetFile);
 });
