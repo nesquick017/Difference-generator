fd3a4032cf94546dc2281e6f9feff59f9ac7cf68
Madorskii <madorskiy.017@yandex.ru>
2023-06-02 23:40:28 -0300

    upd for test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,29 +1,28 @@
 /* eslint-disable no-undef */
 import { noFlat } from '../__fixtures__/result.js';
-import makeCompare from '../src/idex.js';
-import { getFile, getType } from '../src/idex.js';
+import { getType, getFixturePath, getFile } from '../src/idex.js';
+import { fileURLToPath } from 'url';
 import fs from 'fs';
+import makeCompare from '../src/idex.js';
+import path from 'path';
 import getString from '../src/formatters/stylish.js';
 
-const fileName1 = 'file1.json';
-const fileName2 = 'file2.json';
-const resultGetFile = fs.readFileSync(
-    '/Users/nikita_mac/Documents/frontend-project-46/__fixtures__/file1.json',
-    'utf-8'
-);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file1 = fs.readFileSync(getFixturePath('file1.json'), 'utf-8');
+
+test('returns the correct fixture path', () => {
+    const expectedPath = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+    expect(getFixturePath('file1.json')).toBe(expectedPath);
+});
 
 test('makeCompare should return the difference', () => {
-    expect(getString(makeCompare(fileName1, fileName2))).toEqual(noFlat);
+    expect(getString(makeCompare('file1.json', 'file2.json'))).toEqual(noFlat);
 });
 
 test('getType should give us a type of file', () => {
     expect(getType('file1.json')).toEqual('json');
 });
-
-test('readFile should give us a proper file', () => {
-    expect(
-        getFile(
-            '/Users/nikita_mac/Documents/frontend-project-46/__fixtures__/file1.json'
-        )
-    ).toEqual(resultGetFile);
+test('getFile should give us a proper file', () => {
+    expect(getFile(getFixturePath('file1.json'))).toEqual(file1);
 });
--- src/idex.js
+++ src/idex.js
@@ -1,18 +1,23 @@
 /* eslint-disable no-undef */
+import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 import parser from './parser.js';
 import buildTree from './comparer.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getType = (filePath) => path.extname(filePath).slice(1);
 const getFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
-export { getFile, getType };
+export { getFile, getType, getFixturePath };
 
-const makeCompare = (filePath1, filePath2) => {
-    const fullPath1 = path.resolve(process.cwd(), '__fixtures__', filePath1);
-    const fullPath2 = path.resolve(process.cwd(), '__fixtures__', filePath2);
-    const file1 = parser(getFile(fullPath1), getType(fullPath1));
-    const file2 = parser(getFile(fullPath2), getType(fullPath2));
+
+const makeCompare = (fileName1, fileName2) => {
+    const path1 = getFixturePath(fileName1);
+    const path2 = getFixturePath(fileName2);
+    const file1 = parser(getFile(path1), getType(path1));
+    const file2 = parser(getFile(path2), getType(path2));
     return buildTree(file1, file2);
 };
 export default makeCompare;
