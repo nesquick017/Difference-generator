353eda83b0b75663d31f6ff886d38a3c961970f0
Madorskii <madorskiy.017@yandex.ru>
2023-06-05 16:30:22 -0300

    update for plain

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,12 @@
 import stylish from '../formatters/stylish.js';
-
+import plain from './plain.js';
 const formatter = (data, format) => {
     switch (format) {
         case 'stylish': {
             return stylish(data);
+        }
+        case 'plain': {
+            return plain(data);
         }
     }
 };
---
+++ src/formatters/plain.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+const plain = (data) => {
+    const iter = (obj, path = [data.key], depth = 1) => {
+        const result = obj.flatMap((el) => {
+            switch (el.type) {
+                case 'nested': {
+                    return iter(el.children, [el.key], depth + 1);
+                }
+                case 'deleted': {
+                    return `Property ${[...path, el.key].join('.')} was removed`;
+                }
+                case 'added': {
+                    return `Property ${[...path, el.key].join('.')} was added with value: ${el.value} `;
+                }
+                case 'changed': {
+                    return `Property ${[...path, el.key].join('.')} updated from ${el.newValue} to ${el.oldValue}`;
+                }
+                case 'unchanged': {
+                    return [];
+                }
+            }
+        });
+        return result.join('\n');
+    };
+    return iter(data, [data.key], 1);
+};
+export default plain;
