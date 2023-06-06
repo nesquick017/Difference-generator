aa118bfe6aa22241728aae7f93ceada6e02e3432
Madorskii <madorskiy.017@yandex.ru>
2023-06-06 10:59:09 -0300

    discription upd

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,8 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format, <type>', 'output format', 'stylish')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDifference(filepath1, filepath2, program.opts().format));
   })
