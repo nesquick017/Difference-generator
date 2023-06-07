8b21789617328c4ef426f67b046f029aa005226f
Madorskii <madorskiy.017@yandex.ru>
2023-06-06 22:20:16 -0300

    code made more readable

--- src/comparer.js
+++ src/comparer.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
 export default function getComparison(obj1, obj2) {
-  const sortedKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const sortedKeys = _.sortBy(keys);
   const result = sortedKeys.map((key) => {
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { key, type: 'nested', children: getComparison(obj1[key], obj2[key]) };
