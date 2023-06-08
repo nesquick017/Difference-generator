17bded07359b6b7dcb5ec207911bc6ec70a8d1ac
Madorskii <madorskiy.017@yandex.ru>
2023-06-08 18:22:10 -0300

    txt resulst added.

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -5,17 +5,17 @@ import path from 'path';
 import yaml from 'js-yaml';
 import parser from '../src/parser.js';
 import genDifference from '../src/index.js';
-import resultPlain from '../__fixtures__/resultPlain.js';
-import reultJSON from '../__fixtures__/resultJSON.js';
-import resultStylish from '../__fixtures__/resultStylish.js';
 
 const getFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 const getFixturePath = (filepath) => path.resolve(process.cwd(), '__fixtures__', filepath);
+const getTxtResult = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf8');
 
-test('gendiff --format  should return correct result depends on format and extension', () => {
-  expect(genDifference('file1.yml', 'file2.yml')).toEqual(resultStylish);
-  expect(genDifference('file1.json', 'file2.json', 'json')).toEqual(reultJSON);
-  expect(genDifference('file1.yaml', 'file2.yaml', 'plain')).toEqual(resultPlain);
+test('gendiff --format  should return correct string depends on the formatter. Stylish by default.', () => {
+  expect(genDifference('file1.yml', 'file2.yml')).toEqual(getTxtResult('resultStylish.txt'));
+  expect(genDifference('file1.json', 'file2.json', 'json')).toEqual(getTxtResult('resultJSON.txt'));
+  expect(genDifference('file1.yaml', 'file2.yaml', 'plain')).toEqual(
+    getTxtResult('resultPlain.txt'),
+  );
 });
 
 test('parser should return correct file', () => {
